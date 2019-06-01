d0545b3ee41fe5d894ecd7393476d778a3bf53a0
mushfiqulIslam <nnahid878@gmail.com>
2019-06-02 01:26:42 +0600

    Working on the shape

--- Bengali-Calendar/src/bengali/calendar/BengaliCalendar.java
+++ Bengali-Calendar/src/bengali/calendar/BengaliCalendar.java
@@ -6,12 +6,15 @@
 package bengali.calendar;
 
 import java.io.File;
+import java.util.Calendar;
 import java.net.MalformedURLException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -23,20 +26,32 @@ import javafx.stage.Stage;
  */
 public class BengaliCalendar extends Application {
     
+    GridPane buttons = new GridPane();
+    
     @Override
     public void start(Stage primaryStage) throws MalformedURLException {
-        Text text = new Text();
-        text.setText("না");
+        Label l = new Label();
+        l.setText("C");
+        
+        Text t = new Text();
+        t.setText("না");
         Font font = Font.loadFont(new File("Bangla.ttf").toURL().toExternalForm(), 20);
-        text.setFont(font);
+        t.setFont(font);
+        
+        Button b1 = new Button();
+        b1.setText("Button1");
+        b1.prefWidthProperty().bind(buttons.widthProperty());
+        b1.prefHeightProperty().bind(buttons.heightProperty());
+        
+        buttons.addColumn(0, l, b1, t);
         
         
         StackPane root = new StackPane();
-        root.getChildren().add(text);
+        root.getChildren().add(buttons);
         
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 750, 550);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("বাংলা বর্ষপঞ্জি");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
