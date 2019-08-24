6b6aca97c8ece23e1ca06d60663337a8320e142c
mushfiqulIslam <nnahid878@gmail.com>
2019-08-24 23:20:33 +0600

    Calendar View

--- Bengali-Calendar/src/bengali/calendar/BengaliCalendar.java
+++ Bengali-Calendar/src/bengali/calendar/BengaliCalendar.java
@@ -5,22 +5,11 @@
  */
 package bengali.calendar;
 
-import java.io.File;
-import java.util.Calendar;
 import java.net.MalformedURLException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.ContentDisplay;
-import javafx.scene.layout.GridPane;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+import java.time.YearMonth;
 
 /**
  *
@@ -28,47 +17,16 @@ import javafx.stage.Stage;
  */
 public class BengaliCalendar extends Application {
     
-    GridPane buttons = new GridPane();
-    
     @Override
-    public void start(Stage primaryStage) throws MalformedURLException {
+    public void start(Stage primaryStage) throws MalformedURLException{
         
-        Button lbutton = new Button();
-        lbutton.setText("<");
-        
-        Label label = new Label();
-        label.setGraphic(lbutton);
-        label.setContentDisplay(ContentDisplay.RIGHT);
-        
-        label.setText("বৈশাখ");
-        label.setFont(Font.loadFont(new File("Bangla.ttf").toURL().toExternalForm(), 30));
-        label.setTextAlignment(TextAlignment.CENTER);
-        label.setContentDisplay(ContentDisplay.LEFT);
-        
-        label.setMinWidth(50);
-        label.setMinHeight(40);
-        
-        Text t = new Text();
-        t.setText("না");
-        Font font = Font.loadFont(new File("Bangla.ttf").toURL().toExternalForm(), 20);
-        t.setFont(font);
-        
-        Button b1 = new Button();
-        b1.setText("Button1");
-        b1.prefWidthProperty().bind(buttons.widthProperty());
-        b1.prefHeightProperty().bind(buttons.heightProperty());
-        
-        buttons.addColumn(0, label, b1, t);
-        
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(buttons);
-        
-        Scene scene = new Scene(root, 750, 550);
-        
+        //Setting title to the Stage
         primaryStage.setTitle("বাংলা বর্ষপঞ্জি");
-        primaryStage.setScene(scene);
+        
+        //Adding scene to the stage
+        primaryStage.setScene(new Scene(new CalendarView(YearMonth.now()).getView(), 750, 550));
         primaryStage.show();
+        
     }
 
     /**
