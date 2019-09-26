53058b9e8b09624122ed2436b03325b2fa8bf852
mushfiqulIslam <nnahid878@gmail.com>
2019-09-26 23:18:32 +0600

    button functions working

--- Bengali-Calendar/src/bengali/calendar/BengaliCalendar.java
+++ Bengali-Calendar/src/bengali/calendar/BengaliCalendar.java
@@ -10,7 +10,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.time.YearMonth;
 import javafx.scene.Parent;
 import javafx.scene.text.Font;
 
@@ -23,10 +22,13 @@ public class BengaliCalendar extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         
-        //Setting title to the Stage
+        //Loading the fxml file
         Parent root = FXMLLoader.load(getClass().getResource("calenderView.fxml"));
+        //Loading bangla font
         Font.loadFont(new File("Bangla.ttf").toURL().toExternalForm(), 30);
+        //setting title
         primaryStage.setTitle("বাংলা বর্ষপঞ্জি");
+        //adding scene to the stage to load on the frame
         final Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("custom-font-styles.css").toExternalForm());
         primaryStage.setScene(scene);
--- Bengali-Calendar/src/bengali/calendar/CalendarViewController.java
+++ Bengali-Calendar/src/bengali/calendar/CalendarViewController.java
@@ -5,19 +5,19 @@
  */
 package bengali.calendar;
 import java.time.LocalDate;
-import java.time.YearMonth;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.stage.Window;
 /**
  *
  * @author mushfiqul
  */
 public class CalendarViewController{
+    int year;
+    int month;
+    int day;
+    int [] bangla;
+    
     final char[] bn ={'০','১','২','৩','৪','৫','৬','৭','৮','৯'};
     
     //accessing year
@@ -29,9 +29,6 @@ public class CalendarViewController{
         "পৌষ", "মাঘ", "ফাল্গুন", "চৈত্র"};
     final int [] mntLE = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     final int [] mntLB = {31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 30, 30};
-    
-    //tracking last day
-    private String last;
     
     //accessing saterday columns
     @FXML private Label sat0, sat1, sat2, sat3, sat4, sat5;
@@ -54,31 +51,51 @@ public class CalendarViewController{
     //accessing friday columns
     @FXML private Label fri0, fri1, fri2, fri3, fri4, fri5;
     
+    //initializing all the values to load on the frame
     @FXML private void initialize(){
+        //Getting local date as a string and spliting it into corresponding values
         String today = LocalDate.now().toString();
         String [] s = today.split("-");
-        int year = Integer.parseInt(s[0]);
-        int month = Integer.parseInt(s[1]);
-        int day = Integer.parseInt(s[2]);       
+        year = Integer.parseInt(s[0]);
+        month = Integer.parseInt(s[1]);
+        day = Integer.parseInt(s[2]);
+        //getting the right bangla year and adding it into the frame
         populateYear(year, month);
-        int [] bangla = populateMonth(year, month, day);
+        bangla = populateMonth(year, month, day);
         populateDays(bangla[0], bangla[1], year, month, day);
     }
     
     
     
     @FXML protected void leftButton(ActionEvent event){
-        //populateYear(YearMonth.now());
+        if(month <=1){
+            year--;
+            month = 12;  
+        }
+        else
+            month--;
+        
+        if(bangla[0]==1)
+            populateYear(year, month);
+        bangla = populateMonth(year, month, 20);
+        populateDays(bangla[0], bangla[1], year, month, 20);
+        
     }
     
     @FXML protected void rightButton(ActionEvent event){
-        sat0.setText("৫");
-        sun1.setText("৫");
-        mon2.setText("৫");
-        tue3.setText("৫");
-        wed4.setText("৫");
-        thu5.setText("৫");
-        fri3.setText("৫");    
+        if(month >=12){
+            year++;
+            month = 1;
+        }
+        else
+            month++;
+           
+        if(bangla[0]==12)
+            populateYear(year, month);
+        bangla = populateMonth(year, month, 20);
+        populateDays(bangla[0], bangla[1], year, month, 20);
+           
+           
     }
     
     private void populateYear(int year, int month){
@@ -152,26 +169,30 @@ public class CalendarViewController{
             int firstday = engday - (bangladay-1);
             LocalDate calenderdate = LocalDate.of(engyear, engmonth, firstday);
             int start = FindWeek(calenderdate.getDayOfWeek().toString());
-            if(start == 1){
-                populateFirst(banglamonth, engyear);
-            }
-            else if(start == 2){
-                
-            }
-            else if(start == 3){
-                populateThird(banglamonth, engyear);
-            }
-            else if(start == 4){
-                
-            }
-            else if(start == 5){
-                
-            }
-            else if(start == 6){
-                
-            }
-            else if(start == 7){
-                
+            switch (start) {
+                case 1:
+                    populateFirst(banglamonth, engyear);
+                    break;
+                case 2:
+                    populateSecond(banglamonth, engyear);
+                    break;
+                case 3:
+                    populateThird(banglamonth, engyear);
+                    break;
+                case 4:
+                    populateFourth(banglamonth, engyear);
+                    break;
+                case 5:
+                    populateFifth(banglamonth, engyear);
+                    break;
+                case 6:
+                    populateSixth(banglamonth, engyear);
+                    break;
+                case 7:
+                    populateSeventh(banglamonth, engyear);
+                    break;
+                default:
+                    break;
             }
         }
     }
@@ -207,6 +228,7 @@ public class CalendarViewController{
         return builder.toString();
     }
     
+    //finding the exact week name of the day
     private int FindWeek(String s){
         String [] week = {"SATURDAY", "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"};
         for(int i=0;i<week.length;i++){
@@ -214,56 +236,6 @@ public class CalendarViewController{
                 return i+1;
         }
         return 0;
-    }
-
-    //adding days value from monday to the label
-    private void populateThird(int banglamonth, int engyear) {
-        //assinging default days limit 30 as all months after no 5 is 30
-        int days = 30;
-        
-        //assinging days limit 31 as all months before no 5 is 31
-        if(banglamonth<=5){
-            days = 31;
-        }
-        //if its leapyear setting days limit 31 for 11 no month
-        else if((engyear%2==0) && (banglamonth==11)){
-            days = 31;
-        }
-        
-        //setting text in right position
-        mon0.setText("১");
-        tue0.setText("২");
-        wed0.setText("৩");
-        thu0.setText("৪");
-        fri0.setText("৫");
-        sat1.setText("৬");
-        sun1.setText("৭");
-        mon1.setText("৮");
-        tue1.setText("৯");
-        wed1.setText("১০");
-        thu1.setText("১১");
-        fri1.setText("১২");
-        sat2.setText("১৩");
-        sun2.setText("১৪");
-        mon2.setText("১৫");
-        tue2.setText("১৬");
-        wed2.setText("১৭");
-        thu2.setText("১৮");
-        fri2.setText("১৯");
-        sat3.setText("২০");
-        sun3.setText("২১");
-        mon3.setText("২২");
-        tue3.setText("২৩");
-        wed3.setText("২৪");
-        thu3.setText("২৫");
-        fri3.setText("২৬");
-        sat4.setText("২৭");
-        sun4.setText("২৮");
-        mon4.setText("২৯");
-        tue4.setText("৩০");
-        if(days==31){
-            wed4.setText("৩১");
-        }
     }
     
     //adding days value from saturday to the label
@@ -311,11 +283,365 @@ public class CalendarViewController{
         fri3.setText("২৮");
         sat4.setText("২৯");
         sun4.setText("৩০");
+        mon4.setText("");
+        tue4.setText("");
+        wed4.setText("");
+        thu4.setText("");
+        fri4.setText("");
+        sat5.setText("");
+        sun5.setText("");
+        //if we find 31 days month
         if(days==31){
             mon4.setText("৩১");
         }
     }
-
     
+    //adding days value from sunday to the label
+    private void populateSecond(int banglamonth, int engyear) {
+        //assinging default days limit 30 as all months after no 5 is 30
+        int days = 30;
+        
+        //assinging days limit 31 as all months before no 5 is 31
+        if(banglamonth<=5){
+            days = 31;
+        }
+        //if its leapyear setting days limit 31 for 11 no month
+        else if((engyear%2==0) && (banglamonth==11)){
+            days = 31;
+        }
+        
+        //setting text in right position
+        sat0.setText("");
+        sun0.setText("১");
+        mon0.setText("২");
+        tue0.setText("৩");
+        wed0.setText("৪");
+        thu0.setText("৫");
+        fri0.setText("৬");
+        sat1.setText("৭");
+        sun1.setText("৮");
+        mon1.setText("৯");
+        tue1.setText("১০");
+        wed1.setText("১১");
+        thu1.setText("১২");
+        fri1.setText("১৩");
+        sat2.setText("১৪");
+        sun2.setText("১৫");
+        mon2.setText("১৬");
+        tue2.setText("১৭");
+        wed2.setText("১৮");
+        thu2.setText("১৯");
+        fri2.setText("২০");
+        sat3.setText("২১");
+        sun3.setText("২২");
+        mon3.setText("২৩");
+        tue3.setText("২৪");
+        wed3.setText("২৫");
+        thu3.setText("২৬");
+        fri3.setText("২৭");
+        sat4.setText("২৮");
+        sun4.setText("২৯");
+        mon4.setText("৩০");
+        tue4.setText("");
+        wed4.setText("");
+        thu4.setText("");
+        fri4.setText("");
+        sat5.setText("");
+        sun5.setText("");
+        //if we find 31 days month
+        if(days==31){
+            tue4.setText("৩১");
+        }
+    }
+    
+    //adding days value from monday to the label
+    private void populateThird(int banglamonth, int engyear) {
+        //assinging default days limit 30 as all months after no 5 is 30
+        int days = 30;
+        
+        //assinging days limit 31 as all months before no 5 is 31
+        if(banglamonth<=5){
+            days = 31;
+        }
+        //if its leapyear setting days limit 31 for 11 no month
+        else if((engyear%2==0) && (banglamonth==11)){
+            days = 31;
+        }
+        
+        //setting text in right position
+        sat0.setText("");
+        sun0.setText("");
+        mon0.setText("১");
+        tue0.setText("২");
+        wed0.setText("৩");
+        thu0.setText("৪");
+        fri0.setText("৫");
+        sat1.setText("৬");
+        sun1.setText("৭");
+        mon1.setText("৮");
+        tue1.setText("৯");
+        wed1.setText("১০");
+        thu1.setText("১১");
+        fri1.setText("১২");
+        sat2.setText("১৩");
+        sun2.setText("১৪");
+        mon2.setText("১৫");
+        tue2.setText("১৬");
+        wed2.setText("১৭");
+        thu2.setText("১৮");
+        fri2.setText("১৯");
+        sat3.setText("২০");
+        sun3.setText("২১");
+        mon3.setText("২২");
+        tue3.setText("২৩");
+        wed3.setText("২৪");
+        thu3.setText("২৫");
+        fri3.setText("২৬");
+        sat4.setText("২৭");
+        sun4.setText("২৮");
+        mon4.setText("২৯");
+        tue4.setText("৩০");
+        wed4.setText("");
+        thu4.setText("");
+        fri4.setText("");
+        sat5.setText("");
+        sun5.setText("");
+        //if we find 31 days month
+        if(days==31){
+            wed4.setText("৩১");
+        }
+    }
+    
+    //adding days value from tuesday to the label
+    private void populateFourth(int banglamonth, int engyear) {
+        //assinging default days limit 30 as all months after no 5 is 30
+        int days = 30;
+        
+        //assinging days limit 31 as all months before no 5 is 31
+        if(banglamonth<=5){
+            days = 31;
+        }
+        //if its leapyear setting days limit 31 for 11 no month
+        else if((engyear%2==0) && (banglamonth==11)){
+            days = 31;
+        }
+        
+        //setting text in right position
+        sat0.setText("");
+        sun0.setText("");
+        mon0.setText("");
+        tue0.setText("১");
+        wed0.setText("২");
+        thu0.setText("৩");
+        fri0.setText("৪");
+        sat1.setText("৫");
+        sun1.setText("৬");
+        mon1.setText("৭");
+        tue1.setText("৮");
+        wed1.setText("৯");
+        thu1.setText("১০");
+        fri1.setText("১১");
+        sat2.setText("১২");
+        sun2.setText("১৩");
+        mon2.setText("১৪");
+        tue2.setText("১৫");
+        wed2.setText("১৬");
+        thu2.setText("১৭");
+        fri2.setText("১৮");
+        sat3.setText("১৯");
+        sun3.setText("২০");
+        mon3.setText("২১");
+        tue3.setText("২২");
+        wed3.setText("২৩");
+        thu3.setText("২৪");
+        fri3.setText("২৫");
+        sat4.setText("২৬");
+        sun4.setText("২৭");
+        mon4.setText("২৮");
+        tue4.setText("২৯");
+        wed4.setText("৩০");
+        thu4.setText("");
+        fri4.setText("");
+        sat5.setText("");
+        sun5.setText("");
+        //if we find 31 days month
+        if(days==31){
+            thu4.setText("৩১");
+        }
+    }
+    
+    //adding days value from wednessday to the label
+    private void populateFifth(int banglamonth, int engyear) {
+        //assinging default days limit 30 as all months after no 5 is 30
+        int days = 30;
+        
+        //assinging days limit 31 as all months before no 5 is 31
+        if(banglamonth<=5){
+            days = 31;
+        }
+        //if its leapyear setting days limit 31 for 11 no month
+        else if((engyear%2==0) && (banglamonth==11)){
+            days = 31;
+        }
+        
+        //setting text in right position
+        sat0.setText("");
+        sun0.setText("");
+        mon0.setText("");
+        tue0.setText("");
+        wed0.setText("১");
+        thu0.setText("২");
+        fri0.setText("৩");
+        sat1.setText("৪");
+        sun1.setText("৫");
+        mon1.setText("৬");
+        tue1.setText("৭");
+        wed1.setText("৮");
+        thu1.setText("৯");
+        fri1.setText("১০");
+        sat2.setText("১১");
+        sun2.setText("১২");
+        mon2.setText("১৩");
+        tue2.setText("১৪");
+        wed2.setText("১৫");
+        thu2.setText("১৬");
+        fri2.setText("১৭");
+        sat3.setText("১৮");
+        sun3.setText("১৯");
+        mon3.setText("২০");
+        tue3.setText("২১");
+        wed3.setText("২২");
+        thu3.setText("২৩");
+        fri3.setText("২৪");
+        sat4.setText("২৫");
+        sun4.setText("২৬");
+        mon4.setText("২৭");
+        tue4.setText("২৮");
+        wed4.setText("২৯");
+        thu4.setText("৩০");
+        fri4.setText("");
+        sat5.setText("");
+        sun5.setText("");
+        //if we find 31 days month
+        if(days==31){
+            fri4.setText("৩১");
+        }
+    }
+    
+    //adding days value from thursday to the label
+    private void populateSixth(int banglamonth, int engyear) {
+        //assinging default days limit 30 as all months after no 5 is 30
+        int days = 30;
+        
+        //assinging days limit 31 as all months before no 5 is 31
+        if(banglamonth<=5){
+            days = 31;
+        }
+        //if its leapyear setting days limit 31 for 11 no month
+        else if((engyear%2==0) && (banglamonth==11)){
+            days = 31;
+        }
+        
+        //setting text in right position
+        sat0.setText("");
+        sun0.setText("");
+        mon0.setText("");
+        tue0.setText("");
+        wed0.setText("");
+        thu0.setText("১");
+        fri0.setText("২");
+        sat1.setText("৩");
+        sun1.setText("৪");
+        mon1.setText("৫");
+        tue1.setText("৬");
+        wed1.setText("৭");
+        thu1.setText("৮");
+        fri1.setText("৯");
+        sat2.setText("১০");
+        sun2.setText("১১");
+        mon2.setText("১২");
+        tue2.setText("১৩");
+        wed2.setText("১৪");
+        thu2.setText("১৫");
+        fri2.setText("১৬");
+        sat3.setText("১৭");
+        sun3.setText("১৮");
+        mon3.setText("১৯");
+        tue3.setText("২০");
+        wed3.setText("২১");
+        thu3.setText("২২");
+        fri3.setText("২৩");
+        sat4.setText("২৪");
+        sun4.setText("২৫");
+        mon4.setText("২৬");
+        tue4.setText("২৭");
+        wed4.setText("২৮");
+        thu4.setText("২৯");
+        fri4.setText("৩০");
+        sat5.setText("");
+        sun5.setText("");
+        //if we find 31 days month
+        if(days==31){
+            sat5.setText("৩১");
+        }
+    }
+    
+    //adding days value from friday to the label
+    private void populateSeventh(int banglamonth, int engyear) {
+        //assinging default days limit 30 as all months after no 5 is 30
+        int days = 30;
+        
+        //assinging days limit 31 as all months before no 5 is 31
+        if(banglamonth<=5){
+            days = 31;
+        }
+        //if its leapyear setting days limit 31 for 11 no month
+        else if((engyear%2==0) && (banglamonth==11)){
+            days = 31;
+        }
+        
+        //setting text in right position
+        sat0.setText("");
+        sun0.setText("");
+        mon0.setText("");
+        tue0.setText("");
+        wed0.setText("");
+        thu0.setText("");
+        fri0.setText("১");
+        sat1.setText("২");
+        sun1.setText("৩");
+        mon1.setText("৪");
+        tue1.setText("৫");
+        wed1.setText("৬");
+        thu1.setText("৭");
+        fri1.setText("৮");
+        sat2.setText("৯");
+        sun2.setText("১০");
+        mon2.setText("১১");
+        tue2.setText("১২");
+        wed2.setText("১৩");
+        thu2.setText("১৪");
+        fri2.setText("১৫");
+        sat3.setText("১৬");
+        sun3.setText("১৭");
+        mon3.setText("১৮");
+        tue3.setText("১৯");
+        wed3.setText("২০");
+        thu3.setText("২১");
+        fri3.setText("২২");
+        sat4.setText("২৩");
+        sun4.setText("২৪");
+        mon4.setText("২৫");
+        tue4.setText("২৬");
+        wed4.setText("২৭");
+        thu4.setText("২৮");
+        fri4.setText("২৯");
+        sat5.setText("৩০");
+        sun5.setText("");
+        //if we find 31 days month
+        if(days==31){
+            sun5.setText("৩১");
+        }
+    }
     
 }
